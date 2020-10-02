15f9848fdd8564fafd5c86dd79ea0c66dc241386
John Troutman <jtroutmanman@gmail.com>
2020-10-02 18:51:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -73,6 +73,10 @@ class TicTacToe
       pos1 = win_arrays[0]
       pos2 = win_arrays[1]
       pos3 = win_arrays[2]
-      
+      if board[pos1] == "X" && board[pos2] == "X" && board[pos3] == "X" || board[pos1] == "O" && board[pos2] == "O" && board[pos3] == "O"
+      return win_arrays
+      end
+    end
+  return false
   end
 end
